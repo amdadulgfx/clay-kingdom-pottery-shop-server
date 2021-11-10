9d0db5e0ba371efd38e0bfcd035fed1402e53a59
Md Amdadul Haq Arif <79464808+amdadulgfx@users.noreply.github.com>
2021-11-10 07:34:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000
+
+const cors = require('cors');
+require('dotenv').config();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jug4s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// console.log(uri);
+
+async function run() {
+    try {
+
+    }
+
+    finally {
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Clay Kingdom server running successfully!')
+})
+
+app.listen(port, () => {
+    console.log(`listening to the port:`, port)
+})
